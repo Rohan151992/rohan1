33cfce20d5fd2d75153bb16f14d625c1a6d7cfa2
p p <philip@p.com>
2022-05-15 13:02:49 +0000

    CHANGES 3

--- phedrusnew/phedrus3.sql
+++ phedrusnew/phedrus3.sql
@@ -1,3 +1,3 @@
-file 3 line 1 change remote 1  LOCAL COMMIT 3
+file 3 line 1 change remote 1  LOCAL COMMIT 3 CHECK FOR ROLLBACK
 file 3 line 2 change remote phil remote
 file 3 line 3 change remote 222
