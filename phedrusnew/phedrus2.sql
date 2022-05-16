369695b240ce57a009165dbcb6a8ce0eff283ca9
p p <philip@p.com>
2022-05-16 04:47:44 +0000

    wwqeqwe

--- phedrusnew/phedrus2.sql
+++ phedrusnew/phedrus2.sql
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 file 2 line 1 change app conflict LOCAL COMMIT 2 from remoteeeee
+=======
+file 2 line 1 change app conflict LOCAL COMMIT 2 phil the starrrrr
+>>>>>>> stash
 file 2 line 2 change remote
 file 2 line 3 change remote
